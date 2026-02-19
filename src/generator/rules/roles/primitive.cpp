5f0fa1e9907b6599e095bc13f00a6151ea19d043
Till Hofmann <till.hofmann@ml.rwth-aachen.de>
2026-02-19 16:13:07 +0100

    Allow all arg positions in roles

--- src/generator/rules/roles/primitive.cpp
+++ src/generator/rules/roles/primitive.cpp
@@ -8,13 +8,15 @@ void PrimitiveRole::generate_impl(const core::States& states, int target_complex
     assert(target_complexity == 1);
     core::SyntacticElementFactory& factory = data.m_factory;
     for (const auto& predicate : factory.get_vocabulary_info()->get_predicates()) {
-        if (predicate.get_arity() == 2) {
-            auto element = factory.make_primitive_role(predicate, 0, 1);
-            auto denotations = element->evaluate(states, caches);
-            if (data.m_role_hash_table.insert(denotations).second) {
-                std::get<3>(data.m_generated_features).push_back(element);
-                data.m_roles_by_iteration[target_complexity].push_back(std::move(element));
-                increment_generated();
+        for (auto i = 0; i < predicate.get_arity(); i++) {
+            for (auto j = i; j < predicate.get_arity(); j++) {
+                auto element = factory.make_primitive_role(predicate, i, j);
+                auto denotations = element->evaluate(states, caches);
+                if (data.m_role_hash_table.insert(denotations).second) {
+                    std::get<3>(data.m_generated_features).push_back(element);
+                    data.m_roles_by_iteration[target_complexity].push_back(std::move(element));
+                    increment_generated();
+                }
             }
         }
     }
